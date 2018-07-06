9c72c8c01239213e2b8398d5221776d918f76aac
jelee <jelee@postech.ac.kr>
2018-07-06 18:04:52 +0900

    backup-3

--- basic/practice.js
+++ basic/practice.js
@@ -4,7 +4,7 @@ var mediaquery = window.matchMedia("(max-width: 800px)");
 //menuitem 클릭 시 clicked 클래스 추가하기
 var menuitems = document.getElementsByClassName('element');
 var menuitemClicked = function() {
-  for(var j=0;j<menuitems.length;j++){
+  for(var j=0;j<menuitems.length;j++) {
     menuitems[j].className = "element";
   }
   this.className = "element clicked";
@@ -13,51 +13,58 @@ var menuitemClicked = function() {
 for(var i=0;i<menuitems.length;i++) {
   console.log("hello!");
   menuitems[i].addEventListener('click', menuitemClicked, false);
-};
+}
 
 //apps 버튼 클릭 시 nav 보이기, 숨기기
-var apps = document.getElementById('apps');
+var apps = document.getElementsByClassName('apps');
 var navmenu = document.getElementsByClassName('navmenu');
-var appsClicked = function() {
-  if(navmenu[0].style.display != 'none'){
-    navmenu[0].style.display = 'none';
-  }
-  else{ //navmenu 슬라이딩
-    if(mediaquery.matches) {
-      navmenu[0].style.position = 'fixed';
-      navmenu[0].style.top = 40;
-      navmenu[0].style.display = 'block';
-      var pos = 0;
-      var id = setInterval(frame, 10);
-      if(pos == 100) {
-        clearInterval(id);
-      } else {
-        pos++;
-        navmenu[0].style.left = pos + 'px';
-      }
+var mobileNavBody = document.getElementById('mobile');
+var navmenuMobile = document.getElementById('navmenu-mobile');
+
+var appsClicked = function(){
+  if(mediaquery.matches) { //mobile
+    if(mobileNavBody.style.display == 'flex'){
+      mobileNavBody.style.display = 'none';
     }
     else {
-      navmenu[0].style.position = 'static';
-      navmenu[0].style.display = 'block';
+      mobileNavBody.style.display = 'flex';
+      var pos = -240;
+      var id = setInterval(frame, 5);
+      function frame(){
+        if(pos == 0) {
+          clearInterval(id);
+        } else {
+          pos++;
+          mobileNavBody.style.left = pos + 'px';
+        }
+      }
+    }
+    if(navmenu[0].style.display != 'none') {
+      navmenu[0].style.display = 'none';
     }
 
-
-
+  } else { //Desktop
+    if(navmenu[0].style.display != 'none') {
+      navmenu[0].style.display = 'none';
+    } else {
+      navmenu[0].style.display = 'block';
+    }
   }
-
 };
-apps.addEventListener('click', appsClicked, false);
+apps[0].addEventListener('click', appsClicked, false);
+apps[1].addEventListener('click', appsClicked, false);
+
+//mobile에서 검은화면 누르면 navbar 꺼지기
+var mobileBody = document.getElementById('mobile-body');
+var mobileBodyClicked = function(){
+  mobileNavBody.style.display = 'none';
+};
+mobileBody.addEventListener('click', mobileBodyClicked, false);
+
+
 
 //navmenu hover시 scrollbar 보이기
 
-
-
-//Mobile device에서 navmenu over되도록
-function navmenuOver() {
-  if(mediaquery.matches) {
-
-  }
-}
 
 //돋보기 버튼 누르면 검색 창 활성화
 var searchsample = document.getElementById('searchsample');
@@ -69,5 +76,5 @@ var searchexpand = function() {
   topmenuLeft.style.display = 'none';
   topmenuRight.style.display = 'none';
   searchwindow.style.display = 'flex';
-}
+};
 searchsample.addEventListener('click', searchexpand, false);
